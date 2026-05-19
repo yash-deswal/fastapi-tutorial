7f1deffbf12ba4141d801ed9396b36d5d93c0108
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-19 16:21:06 +0530

    Update api with 3 routes

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Home Route
 @app.get("/")
 def home():
-    return {"message":"Hello world from FastAPI"}
+    return {"message":"Welcome to FastApi"}
+
+#About Route
+@app.get("/about")
+def about():
+    return {"message":"This is About Page"}
+
+#Users Route
+@app.get("/users")
+def users():
+    return {
+        "users":["Mohit","Rohit","Amit"]
+    }
