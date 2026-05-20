25cd6a81e7abba79b2844041cc047f0487fb6aed
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-20 16:27:29 +0530

    Users route with dynamic routing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+#Users Route
+@app.get("/users/{user_id}")
+def get_user(user_id):
+    return {"user_id": user_id}
