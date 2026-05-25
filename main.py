1973990252ce8ea8797eae91cdddfa4b168c06ae
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-25 22:40:03 +0530

    Intro to POST

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/create-user")
+def create_user(user: User):
+    return {
+        "message": "User created",
+        "data": user
+    }
