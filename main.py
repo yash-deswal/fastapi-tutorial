6b0cb0cbfda22f574adf235fc0190b3378d61941
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-28 12:23:07 +0530

    Pydantic and nested model

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+# class User(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+
+# @app.post("/create_user")
+# def create_user(user: User):
+#     return {
+#         "message": "User Created",
+#         "data": user
+#     }
+
+class Address(BaseModel):
+    city: str
+    pincode: int
+
+class User(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+@app.post("/create_user")
+def create_user(user: User):
+    return {
+        "message": "User Created",
+        "data": user
+    }
