10729599b05e49c865101a5df7ad9959b949d8d2
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-31 01:49:11 +0530

    Add crud

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+todos = []
+
+class Todo(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"Message": "ToDo Added", "data":todo}
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"error": "Todo not found"}
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: Todo):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[index] = updated_todo
+            return {
+                "message": "Data Updated",
+                "data": updated_todo
+            }
+    return {"error": "Todo not found"}
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos.pop(index)
+            return {"message": "Data deleted"}
+    return {"error": "Todo not found"}
