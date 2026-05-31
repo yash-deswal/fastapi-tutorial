3dabfd012483bdc6ac472970d5d42bcd949c8056
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-31 13:06:51 +0530

    path query body

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+users = []
+
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/users")
+def create_user(user: User):
+    users.append(user)
+    return {"message": "User created", "data": user}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User, notify: bool = False):
+    if user_id < len(users):
+        users[user_id] = user
+        return {"message": "User updated", "notify": notify, "data": user}
+    return {"error": "User not found"}
