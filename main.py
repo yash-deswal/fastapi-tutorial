b6405899d643d7572b5e97b6e366d9ac8328897e
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-06-13 21:40:38 +0530

    Exceptional Handling

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+class UserNotFoundException(Exception):
+    def __init__(self, name:str):
+        self.name = name
+
+@app.exception_handler(UserNotFoundException)
+def user_not_found_handler(request: Request, exc:UserNotFoundException):
+    return JSONResponse(
+        status_code= 404,
+        content={
+            "status": "error",
+            "message": f"User {exc.name} not found"
+        }
+    )
+
+@app.get("/user/{name}")
+def get_user(name: str):
+    if name != "Mohit":
+        raise UserNotFoundException(name)
+    return {
+        "name": name
+    }
+
+# @app.get("/users/{user_id}")
+# def get_user(user_id: int):
+#     if user_id != 1:
+#         raise HTTPException(
+#             status_code= 404,
+#             detail= "User not found"
+#         )
+#     return {
+#         "id": 1,
+#         "name": "Mohit"
+#     }
