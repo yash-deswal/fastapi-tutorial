186b61bbf8ce9a9f78286285e9e4e0ada1bb973c
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-06-15 07:45:21 +0530

    Dependency Injection

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Header, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+def verify_token(token: str = Header(None)):
+    if token != "mysecrettoken":
+        raise HTTPException(
+            status_code= 401,
+            detail= "Unauthorized"
+        )
+    return {
+        "user": "Authorized user"
+    }
+
+@app.get("/secure-data")
+def secure_data(user = Depends(verify_token)):
+    return {
+        "message": "secure data accessed",
+        "user": user
+    }
+
+# def common_logic():
+#     return {
+#         "message": "Common logic executed"
+#     }
+
+# @app.get("/home")
+# def home(data = Depends(common_logic)):
+#     return data
+
+# def get_current_user():
+#     return {
+#         "user": "Yash"
+#     }
+
+# @app.get("/profile")
+# def profile(user = Depends(get_current_user)):
+#     return user
+
+# @app.get("/dashboard")
+# def dashboard(user = Depends(get_current_user)):
+#     return user
+
+
