45a5b09f868a2553271b4c75e6577c3cf5c2daa0
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-06-18 01:25:19 +0530

    middleware

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import time
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message":"Hello world from FastAPI"}
+# @app.middleware("http")
+# async def my_middleware(request: Request, call_next):
+#     print("Request received")
+#     response = await call_next(request)
+#     print("Response sent")
+#     return response
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    print(f"Path{request.url.path} | Time:{duration}")
+    return response
