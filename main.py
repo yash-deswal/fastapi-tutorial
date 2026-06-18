352b98570d6bbb8251309a9b225d8bbcbacdf644
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-06-18 11:27:29 +0530

    SQLite DB

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import sqlite3
 from fastapi import FastAPI
 
 app = FastAPI()
+connect = sqlite3.connect('example.db', check_same_thread=False)
+cursor = connect.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        completed TEXT
+    )
+""")
+
+connect.commit()
 
 @app.get("/")
 def home():
-    return {"message":"Hello world from FastAPI"}
+    return {
+        "message": "SQLite connected fine"
+    }
